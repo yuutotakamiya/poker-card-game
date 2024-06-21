25ee19bd2c14fe087972f06aac27e21e43f55a07
S-JOBI23-083\student <231065@st.yoshida-g.ac.jp>
2024-06-21 09:22:28 +0900

    VisualStudio2022でプロジェクト作成 #1

---
+++ Poker-game/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1～4の数字を4つ入力してください");
+
+            // 数値の入力を取得
+            int[] numbers = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"{i + 1}番目のカードを入力してください：");
+                    if (int.TryParse(Console.ReadLine(), out int num) && num >= 1 && num <= 4)
+                    {
+                        numbers[i] = num;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("1～4の間の有効な数字を入力してください。");
+                    }
+                }
+            }
+        }
+    }
+}
